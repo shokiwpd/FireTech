1e7ef43f54e141139f788ea95c035b4ca8531bd9
Pavel Ulanov <shoki-bmx@yandex.ru>
2022-11-13 22:14:40 +0400

    Доработка экрана авторизации, добавление фреймворков
    -Добавление фреймворка
    -Настройка полей ввода текста
    -Добавление фильтра ввода текста
    -Добавление таб бара для работы с переходами
    -Добавление комментариев

--- FireTech/ViewControllerGroup/AuthorizationViewController.swift
+++ FireTech/ViewControllerGroup/AuthorizationViewController.swift
@@ -10,7 +10,8 @@ import UIKit
 
 
 class AuthorizationViewController: UIViewController {
-    //Объявление элементов интерфейса
+    
+    //MARK: Объявление элементов интерфейса
     let appNameLabel = headingLabel()
     let viewContainer = customViewContainer()
     let loginField = loginTextField()
@@ -18,59 +19,112 @@ class AuthorizationViewController: UIViewController {
     let loginButton = appActiveButton()
     let OfflineLoginButton = appClearButton()
 
-    
     override func viewDidLayoutSubviews() {
-        createLayoutUIElement()//Задаем положение элементов на экране
+        //MARK: Задаем положение элементов на экране
+        createLayoutUIElement()
     }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        print(loadWidthScreenSize())
-        //Создаем элементы интерфейса
+        //MARK: Создаем элементы интерфейса
         self.view.creatingElements([appNameLabel, viewContainer])
         self.viewContainer.creatingElements([loginField, passwordField, loginButton, OfflineLoginButton])
-        //Задаем цвет фона
+        //MARK: Задаем цвет фона
         view.backgroundColor = UIColor.colorBackgroundView
-        //Присваиваем названия кнопок
+        //MARK: Присваиваем названия кнопок
+        loginField.delegate = self
+        passwordField.delegate = self
         loginButton.setTitle("Войти", for: .normal)
         OfflineLoginButton.setTitle("Оффлайн режим", for: .normal)
-        //Задаем размер шрифта исходя из размера экрана
+        //MARK: Задаем размер шрифта исходя из размера экрана
         appNameLabel.headingfontStyle(String(loadScreenSize()))
+        addTarget()
     }
     
-    //Расстановка элементов (Код надо будет распиать более аккуратно и компактно)
+    //MARK: Расстановка элементов (Код надо будет распиать более аккуратно и компактно)
+    func addTarget(){
+        loginButton.addTarget(self, action: #selector(userAuthorizationFunction), for: .touchDown)
+        OfflineLoginButton.addTarget(self, action: #selector(registrationAlert), for: .touchDown)
+    }
+    
+    //MARK: Функция расставляющая элементы внутри контроллера
     func createLayoutUIElement() {
         appNameLabel.positionOfElements(nil, 0,
                                         view.leadingAnchor, 20,
                                         view.trailingAnchor, -20,
                                         viewContainer.topAnchor, -50)
-        //Задаем положение и размеры контейнера
+        //MARK: Задаем положение и размеры контейнера
         viewContainer.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
         viewContainer.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         viewContainer.widthSize(UIScreen.main.bounds.width - 100)
         viewContainer.heightSize(UIScreen.main.bounds.width - 100)
-        
+        //MARK: Настройка расположения поля ввода логина
         loginField.positionOfElements(viewContainer.topAnchor, ((loadWidthScreenSize()-100) - ((((loadWidthScreenSize()-100)/8)*4)+65))/2,
                                       viewContainer.leadingAnchor, 10,
                                       viewContainer.trailingAnchor, -10,
                                       nil, 0)
         loginField.heightSize((loadWidthScreenSize()-100)/8)
-        //
+        //MARK: Настройка расположения поля ввода пароля
         passwordField.positionOfElements(loginField.bottomAnchor, 15,
                                          loginField.leadingAnchor, 0,
                                          loginField.trailingAnchor, 0,
                                          nil, 0)
         passwordField.heightSize((loadWidthScreenSize()-100)/8)
-        //
+        //MARK: Настройка расположения кнопки авторизации
         loginButton.positionOfElements(passwordField.bottomAnchor, 15,
                                        passwordField.leadingAnchor, 0,
                                        passwordField.trailingAnchor, 0,
                                        nil, 0)
         loginButton.heightSize(((loadWidthScreenSize() - 100)/8)+10)
-        //
+        //MARK: Настройка расположения кнопки оффлайн режима
         OfflineLoginButton.positionOfElements(loginButton.bottomAnchor, 15,
                                               loginButton.leadingAnchor, 0,
                                               loginButton.trailingAnchor, 0,
                                               nil, 0)
         OfflineLoginButton.heightSize(((loadWidthScreenSize()-100)/8)+10)
     }
+    
+    //MARK: проверка полей на наличие текста (При подключении к БД будет обработка вода и ошибок)
+   @objc func userAuthorizationFunction(){
+       if loginField.text! != "" {
+           if passwordField.text! != "" {
+               print("User name: \(loginField.text!) select password \(passwordField.text!)")
+            }
+        }
+    }
+
+    //MARK: Функция которая предупреждает пользователя о регистрации для сохранения данных в облаке
+   @objc func registrationAlert() {
+        let alertConttroller = UIAlertController(title: "Внимание", message: "Без регистрации все данные в приложении будут удалены после переустановки. Хотите продолжить?", preferredStyle: .alert)
+       alertConttroller.addAction(UIAlertAction.init(title: "Регистрация", style: .default))
+       alertConttroller.addAction(UIAlertAction.init(title: "Продолжить без регистрации", style: .destructive, handler: { [self] (action) in
+            offlineWork()
+        }))
+        present(alertConttroller, animated: true)
+    }
+    
+    //MARK: функция переход на следующий рабчоий экран
+    func offlineWork(){
+        let openTabbar = mainTabBarController()
+        openTabbar.modalTransitionStyle = .crossDissolve
+        openTabbar.modalPresentationStyle = .fullScreen
+        present(openTabbar, animated: true, completion: nil)
+    }
+}
+
+    //MARK: Настройка полей ввода текста(Пока примерный фильтр на поля)
+extension AuthorizationViewController: UITextFieldDelegate {
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        var filterString = ""
+        if textField == loginField {
+            filterString = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz@.()_-"
+        }
+        if textField == passwordField {
+            filterString = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890"
+        }
+        // remove non-numerics and compare with original string
+        return string == string.filter(filterString.contains)
+
+    }
+    
 }
---
+++ Podfile
@@ -0,0 +1,3 @@
+target 'FireTech' do
+    pod 'MBProgressHUD', '~> 1.2.0'
+end
